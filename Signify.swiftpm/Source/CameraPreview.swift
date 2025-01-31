088f14e5ca31406b19e29191ee0c419d0ecc48bd
dbsghdz1 <dbsghdz1@gmail.com>
2025-01-31 22:21:04 +0900

    카메라 다루기 추가

--- Signify.swiftpm/Source/CameraPreview.swift
+++ Signify.swiftpm/Source/CameraPreview.swift
@@ -8,10 +8,16 @@
 import Foundation
 import AVFoundation
 import SwiftUI
+import Vision
 
 struct CameraPreviewView: UIViewRepresentable {
     
     private var captureSession = AVCaptureSession()
+    private let requests = [VNRequest]()
+    private let queue = DispatchQueue(label: "VideoDataOutput",
+                                      qos: .userInitiated,
+                                      attributes: [],
+                                      autoreleaseFrequency: .workItem)
     
     func makeUIView(context: Context) -> UIView {
         let view = PreviewView()
@@ -45,23 +51,68 @@ struct CameraPreviewView: UIViewRepresentable {
     func setCamera() async {
         guard await isAuthorized else { return }
         
-        captureSession.beginConfiguration()
         let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
                                                   for: .video,
                                                   position: .front)
+        
+        captureSession.beginConfiguration()
+        captureSession.sessionPreset = .photo
+        
         guard
             let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
             captureSession.canAddInput(videoDeviceInput)
             else { return }
         captureSession.addInput(videoDeviceInput)
         
-        let photoOutput = AVCapturePhotoOutput()
-        guard captureSession.canAddOutput(photoOutput) else { return }
-        captureSession.sessionPreset = .photo
-        captureSession.addOutput(photoOutput)
-        captureSession.commitConfiguration()
+        let output = AVCaptureVideoDataOutput()
+        
+        if captureSession.canAddOutput(output) {
+            captureSession.addOutput(output)
+            output.alwaysDiscardsLateVideoFrames = true
+            output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+//            output.setSampleBufferDelegate(self, queue: queue)
+        } else {
+            captureSession.commitConfiguration()
+            return
+        }
         
         captureSession.startRunning()
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput,
+                       didOutput sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        let exifOrientation = exifOrientationFromDeviceOrientation()
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: exifOrientation, options: [:])
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    public func exifOrientationFromDeviceOrientation() -> CGImagePropertyOrientation {
+        let curDeviceOrientation = UIDevice.current.orientation
+        let exifOrientation: CGImagePropertyOrientation
+        
+        switch curDeviceOrientation {
+        case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
+            exifOrientation = .left
+        case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
+            exifOrientation = .upMirrored
+        case UIDeviceOrientation.landscapeRight:      // Device oriented horizontally, home button on the left
+            exifOrientation = .down
+        case UIDeviceOrientation.portrait:            // Device oriented vertically, home button on the bottom
+            exifOrientation = .up
+        default:
+            exifOrientation = .up
+        }
+        return exifOrientation
     }
 
 }
