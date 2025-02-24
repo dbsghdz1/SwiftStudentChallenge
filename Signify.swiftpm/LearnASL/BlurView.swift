85a9d23bc5271f9bb9144b5fec38865c9777acda
dbsghdz1 <dbsghdz1@gmail.com>
2025-02-24 10:38:02 +0900

    Feat: blurView

---
+++ Signify.swiftpm/LearnASL/BlurView.swift
@@ -0,0 +1,19 @@
+//
+//  BlurView.swift
+//  Signify
+//
+//  Created by 김윤홍 on 2/24/25.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {}
+}
